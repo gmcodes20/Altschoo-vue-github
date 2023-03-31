032c312aaecdb5ff00d6be577ba7df3bf2e2f0b7
gmcodes20 <gmcodes20@gmail.com>
2023-03-31 23:04:28 +0100

    Edit About page

--- src/components/AboutApp.vue
+++ src/components/AboutApp.vue
@@ -1,15 +1,28 @@
 <template>
   <main>
     <h1>About</h1>
+    <h3>Question</h3>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque magni est
-      nemo molestiae alias esse dolorum nam placeat ipsum aliquam voluptates
-      voluptate laborum, doloribus eaque temporibus velit labore exercitationem
-      a, minima illo. Non enim molestias rem at labore amet reprehenderit ipsum
-      perferendis illo, fugit cupiditate iusto corrupti exercitationem, maiores
-      eveniet.
+      Create a new vue js application then using the GitHub API, fetch your
+      GitHub repositories using this endpoint
+      https://api.github.com/users/`USERNAME`/repos. show a page with a list of
+      all your repositories on GitHub(the page should have some form of
+      pagination). Then create another page showing data for a single repo
+      clicked from the list of repos using nested routes. Make sure to implement
+      a wildcard route to catch 404 pages. NB: Good UI and Designs are
+      important.
     </p>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+h3 {
+  font-size: 1.8rem;
+  padding-top: 1em;
+}
+
+p {
+  padding-top: 1em;
+  font-size: 1.4rem;
+}
+</style>
